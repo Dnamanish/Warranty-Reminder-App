85eaf15878be89938c71f397000e5568262d1a39
Dnamanish <lucifierop@gmail.com>
2025-12-09 13:37:12 +0530

    Updated cors

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,40 @@
-const express=require('express')
-const app=express()
-require('dotenv').config()
-require('./models/Db')
-const bodyParser=require('body-parser')
-const cors=require('cors')
-const AuthRouter=require('./Routes/AuthRouter')
-const UploadRouter=require('./Routes/UploadsRoutes')
-const warranty = require('./models/warranty')
-require('./reminderCron')
+const express = require("express");
+const app = express();
+require("dotenv").config();
+require("./models/Db");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const AuthRouter = require("./Routes/AuthRouter");
+const UploadRouter = require("./Routes/UploadsRoutes");
+const warranty = require("./models/warranty");
+require("./reminderCron");
 
-app.use('/uploads', express.static('uploads'));
+app.use("/uploads", express.static("uploads"));
 
-const PORT= process.env.PORT || 8080;
-app.use(bodyParser.json())
+const PORT = process.env.PORT || 8080;
+app.use(bodyParser.json());
 
-const allowedOrigins = [
-  'http://localhost:3000', 
-  'https://warranty-reminder-app.vercel.app/' 
-];
-app.use(cors())
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:5173", 
+      "https://warranty-reminder-app.vercel.app/", 
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
-app.use('/auth', AuthRouter)
+app.use("/auth", AuthRouter);
 
 // app.get('/',(req,res)=>{
 //     res.send('pong')
 // })
 
-app.use('/dashboard',UploadRouter);
+app.use("/dashboard", UploadRouter);
 
-
-
-app.listen(PORT,()=>{
-    console.log(`server is running ${PORT}`);
-    
-})
+app.listen(PORT, () => {
+  console.log(`server is running ${PORT}`);
+});
